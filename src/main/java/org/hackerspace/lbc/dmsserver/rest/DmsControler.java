bd4b296bf97ecc3272399fdba037f1a0d1dd46f4
Kevin <kevind@backbase.com>
2021-04-19 23:28:32 +0000

    generating openapi clients for resttemplates

--- src/main/java/org/hackerspace/lbc/dmsserver/rest/DmsControler.java
+++ src/main/java/org/hackerspace/lbc/dmsserver/rest/DmsControler.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DmsControler {
+public class DmsControler   {
 
     @GetMapping(path = "/hello")
     public String getHello (){
